c1379567a437b2e2232dbfbce19d44e340150930
William Parlan <williamparlan@gmail.com>
2020-12-09 23:42:12 -0800

    Initial Commit

---
+++ pa8-wparlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pa8-wparlan
+//
+//  Created by Parlan, William C on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
